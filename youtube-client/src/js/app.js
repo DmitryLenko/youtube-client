2864590791c3eb930296149604490626087e9bb7
DmitryLenko <dias1381254@gmail.com>
2018-11-26 00:24:53 +0300

    refactor: remove extra files

--- youtube-client/src/js/app.js
+++
@@ -1,6 +0,0 @@
-import SearchBar from './components/searchbar';
-import '../css/style.css';
-
-const search = new SearchBar();
-document.body.classList.add('bg-body');
-document.body.appendChild(search);
